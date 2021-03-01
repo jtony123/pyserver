9c70dc63ecab517e04b515423427d13fe982df6e
jtony123 <jtony123@gmail.com>
2021-03-01 20:49:41 +0000

    second sensor added

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import jsonify
 #from flask_restful import Resource, Api
 from flask_cors import CORS, cross_origin
 import xml.parsers.expat
@@ -15,26 +16,44 @@ import busio
 from busio import SPI
 import adafruit_max31855
 import time
+from datetime import datetime, timedelta
+
 
 app = Flask(__name__)
 cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+#app.config['CORS_HEADERS'] = 'Content-Type'
 #api = Api(app)
 
-spi = SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
-cs = digitalio.DigitalInOut(board.D5)
-sensor = adafruit_max31855.MAX31855(spi, cs);
+spi_a = SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+spi_b = SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+cs_a = digitalio.DigitalInOut(board.D5)
+cs_b = digitalio.DigitalInOut(board.D6)
+
+
+sensorA = adafruit_max31855.MAX31855(spi_a, cs_a,)
+sensorB = adafruit_max31855.MAX31855(spi_b, cs_b)
 
 @app.route('/', methods=['GET'])
 def index():
-	return str(round(sensor.temperature, 1))
+
+    data = {"sensorA": str(round(sensorA.temperature, 1)), "sensorB": str(round(sensorB.temperature, 1))}
+    return jsonify(data)
 
 @app.route('/weather', methods=['GET'])
 def weather():
 
+    x = datetime.now()
+    y = x - timedelta(hours=4, minutes=0)
+    param1 = y.strftime("%Y-%m-%d"+"T"+"%H:%M")
+    z = x + timedelta(hours=24, minutes=0)
+    url = 'http://metwdb-openaccess.ichec.ie/metno-wdb2ts/locationforecast?lat=53.392650;long=-9.271570;from='
+    param1 = y.strftime("%Y-%m-%d"+"T"+"%H:%M")
+    param2 = z.strftime("%Y-%m-%d"+"T"+"%H:%M")
+    fullUrl = url + param1 + ";to=" + param2
+
     #response.headers["Content-Type"] = "application/json"
-    r = requests.get('http://metwdb-openaccess.ichec.ie/metno-wdb2ts/locationforecast?lat=53.392650;long=-9.271570')
-    print (r.status_code)
+    r = requests.get( fullUrl )
+    #print (r.status_code)
 
     # def first_element(tag, attrs):
     #     print ('first element:', tag, attrs)
